9bbe535a35c22b327c11e2c14ab7b986e00f8886
JuanesTorres <jetorress@academia.usbbog.edu.co>
2025-09-20 06:31:35 +0000

    update

--- PedidoComplementado.c
+++ PedidoComplementado.c
@@ -5,61 +5,59 @@ int main() {
     char seguir;
     int total = 0;
 
-    printf("Ingresa un número del 1 al 5 dependiendo el producto que quieras seleccionar.\ncual es tu número?: ");
-    scanf("%d", &Producto);
-
     do {
-        scanf("ESTE ES EL MENU DEL DIA DE HOY!!!!!");
-        printf("1. Agua con gas ");
-        printf("2. Gaseosa ");
-        printf("3. Salchipapa ");
-        printf("4. Hamburguesa ");
-        printf("5. Postre de fresa ");
-        printf("6. Postre de mora ");
-        scanf("Ingresa un número del 1 al 5 dependiendo el producto que quieras seleccionar.\ncual es tu número?: ");
+        printf("ESTE ES EL MENU DEL DIA DE HOY!!!!!\n");
+        printf("1. Agua con gas \n");
+        printf("2. Gaseosa \n");
+        printf("3. Salchipapa \n");
+        printf("4. Hamburguesa \n");
+        printf("5. Postre de fresa \n");
+        printf("6. Postre de mora \n");
+        printf("Ingresa un número del 1 al 6 dependiendo el producto que quieras seleccionar.\ncual es tu número?: ");
         scanf("%d", &Producto);
     
         switch (Producto) {
             case 1:
                 printf("Elegiste un agua con gas\n");
                 printf("El valor de tu agua con gas es 2000 pesos");
+                total+=2000;
                 break;
             case 2:
                 printf("Elegiste una gaseosa\n");
                 printf("El valor de la gaseosa es de 3000 pesos");
+                total+=3000;
                 break;
             case 3:
                 printf("Elegiste una salchipapa\n");
                 printf("El valor de la salchipapa es de 30000 pesos");
+                total+=30000;
                 break;
             case 4:
                 printf("Elegiste una hamburguesa\n");
                 printf("El valor de la hamburguesa es de 15000 pesos");
+                total+=15000;
                 break;
             case 5:
                 printf("Elegiste un postre de fresa\n");
                 printf("El valor del postre es de 10000 pesos ");
+                total+=10000;
                 break;
             case 6:
                 printf("Elegiste un postre de mora\n");
                 printf("El valor del postre es de 10000 pesos ");
+                total+=10000;
                 break;
             default:
-            printf("Número fuera de rango, por favor ingresa un número entre 1 y 5.\n");
+                printf("Número fuera de rango, por favor ingresa un número entre 1 y 6.\n");
             
-            continue;
+                continue;
+        }
+        printf("deseas poner mas productos en tu pedido (s/n): ");ad
+        scanf(" %c", &seguir);  
+
+    } while (seguir == 's'|| seguir == 'S');
+
+        printf("\nGracias por el pedido, el total de tu factura es %d pesos\n", total);
         
-        printf("deseas poner mas productos en tu pedido?")
-        }while(seguir == 's'| seguir == 'S');
-        printf("\nGracias por el pedido, el total de tu factura es %d pesos");
-        return 0;
-    
-        
-    
-    
-
-
-
-
-
+    return 0;
 } 
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Factura-LS
+# 📌 Pasos para realizar el código del pedido
+
+1. **Declaramos las variables que necesitamos**  
+2. **Utilizamos un `do while`**  
+3. **Dentro del `do` pondremos todos los `printf`** que queremos que el usuario vea  
+4. **Dentro del `do` también pondremos un `switch` del producto**, en cada `case` tendremos:  
+   - El producto elegido  
+   - El precio del producto  
+   - La operación para sumar todos los productos que pida el cliente  
+5. **Para poder hacer el `while` agregamos un `printf`** en el que se le pregunta al usuario si quiere seguir agregando productos.  
+   - Si el usuario selecciona la letra **S/s**, puede seguir añadiendo productos a la orden.  
+   - Si pone **N** o algo diferente a **S/s**, finaliza la condición o como tal el proceso.  
+6. **Ya para finalizar y mostrarle al usuario cuánto debe pagar**, generamos un `printf` en el cual se agradece por el pedido y se muestra el total a pagar.  
